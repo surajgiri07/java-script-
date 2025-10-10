511e1acb934e0c65017be92abb52304ecb9ede7c
surajgiri07 <sg7131777@gmail.com>
2025-10-10 09:13:33 +0000

    data types done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+let name= "suraj"  //string
+let age= 18  //number
+let loggedIn=true //boolean (checks true or false)
+let temperature=null //null
+let city //undefined
+
+//string=""
+
+
+console.log(typeof "suraj")
+console.log(typeof age);//we can find the datatypes of variable and values by using typeof
