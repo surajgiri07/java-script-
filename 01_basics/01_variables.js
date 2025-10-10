fef6558aefdb595c8a48516fb5eada220575447d
surajgiri07 <sg7131777@gmail.com>
2025-10-10 08:41:50 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId="7777" // constant is use to give constant value to a variables its value cannot change
+var accountEmail="suraj@gmail.com"//we donot use this 
+let accountCity="pokhara"//mainly used
+accountPassword="1234"
+let accountState
+
+console.log(accountId);
+//accountId="2" is not allowed
+accountCity="kathmandu"
+accountEmail="sg@gmail.com"
+accountPassword="2345"
+console.table([accountCity,accountEmail,accountState,accountPassword]);
