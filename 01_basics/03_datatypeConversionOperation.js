c9f089c661dbd361d1c26fc6e48824711cc3dba5
surajgiri07 <sg7131777@gmail.com>
2025-10-10 11:21:33 +0000

    data type conversion

---
+++ 01_basics/03_datatypeConversionOperation.js
@@ -0,0 +1,17 @@
+let score="A"//string 
+
+console.log(typeof score);
+
+let valueInNumber=Number(score) //converting string into number
+console.log(typeof valueInNumber);
+ 
+console.log(valueInNumber);
+//NaN= not a number
+//if value of score was null then valueInNumber would be zero.
+//true=1
+//false=0
+//""=false
+//"suraj"=true
+let value=1
+let booleanValue=Boolean(value)
+console.log(booleanValue);
