b957453a402abc2ef95e7a743860f89477d140f2
surajgiri07 <sg7131777@gmail.com>
2025-10-17 08:53:53 +0000

    done

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,12 @@
+const balance = new Number(200)
+console.log(balance);
+
+console.log(balance.toString().length);//toString converts num to string and lenght shows the length of string
+console.log(balance.toFixed(1));
+
+const value=777.5098
+console.log(value.toPrecision(3)
+
+//++==============Maths=======================//
+
+console.log(Math.abs(-5));
