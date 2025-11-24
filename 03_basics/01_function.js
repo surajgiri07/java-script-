1b85fa1b5d34c8f990ad052bd60112dfb86e229f
Suraj Giri <sg7131777@gmail.com>
2025-11-24 12:31:06 +0000

    done

---
+++ 03_basics/01_function.js
@@ -0,0 +1,18 @@
+function MyCv(){
+     let name="suraj"
+     let gender="male"
+     let age=18
+
+    console.log(name)
+}
+
+MyCv()
+
+function add(num1,num2){
+    //console.log(num1+num2)
+    //let result=num1+num2
+    //console.log(result)
+    return num1+num2
+}
+//add(8,9)
+console.log(add(8,9))
